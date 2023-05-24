5d7c54c374fd791cd2706bd727596b053887dad6
Skindetafenbolle <elited013@gmail.com>
2023-05-24 09:06:51 +0300

    =new init

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -14,14 +14,14 @@ module.exports = createCoreController("api::order.order", ({ strapi }) => ({
         products.map(async (product) => {
           const item = await strapi
             .service("api::product.product")
-            .findOne(product.id)
+            .findOne(product.id);
 
           console.log("this is item------->", item);
           console.log("this is product------->", product);
 
           return {
             price_data: {
-              currency: "byn",
+              currency: "usd",
               product_data: {
                 name: item.name,
               },
